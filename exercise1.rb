df29da0a0715993fcfdba28c9d6486e510c56a9c
noah tenser <ntenser@hotmail.com>
2017-01-16 15:11:32 -0500

    Starting exercise3

--- exercise1.rb
+++ exercise1.rb
@@ -3,3 +3,10 @@ puts 2 + 3
 puts 2 
 puts 3
 puts 2 + 3
+
+puts "Ada Lovelace live for #{1852 - 1815} years."
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+# Exercise 2
+
+# Question 1 - How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer.
+puts 55 * 0.15
+
+# Question 2 - Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result.
+puts "Hello" "5"
+
+# Question 3 - Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+"I am trying to calculate #{45628 * 7839} for ever."
+
+# Question 5 - What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
+puts true
+puts (10 < 20 && 30 < 20) || !(10 == 11)
